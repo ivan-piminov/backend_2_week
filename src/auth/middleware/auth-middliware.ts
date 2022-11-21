c928efbd09bba552879432eb649abd68e5a4d9a1
ivan <ivan-piminov@yandex.ru>
2022-11-21 22:20:15 +0300

    changed import

--- src/auth/middleware/auth-middliware.ts
+++ src/auth/middleware/auth-middliware.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response} from "express";
-import {HTTP_STATUSES} from "../../index";
+import {HTTP_STATUSES} from "../../helpers/HTTP-statuses";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     return req.headers.authorization === 'Basic YWRtaW46cXdlcnR5'
