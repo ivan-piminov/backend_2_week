8443a80747a81714b23d92d622c9cfc90e85b125
ivan <ivan-piminov@yandex.ru>
2022-12-02 21:00:56 +0300

    refactoring

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -97,6 +97,7 @@ postsRouter.put(
         }),
     inputValidationMiddleware,
     async (req: Request, res: Response) => {
+        if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
         let post = await postRepository.updatePost(
             req.params.id,
             req.body.title,
