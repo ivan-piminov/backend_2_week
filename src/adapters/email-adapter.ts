2b4f3e58093c14407cb392bf2e591e3cefa7cce8
ivan <ivan-piminov@yandex.ru>
2022-12-20 23:00:17 +0300

    refactoring

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -20,7 +20,7 @@ export const EmailAdapter = {
       subject,
       html: `<h1>Thank for your registration</h1>
        <p>To finish registration please follow the link below:
-          <a href="https://somesite.com/confirm-email?code=' + code + '">complete registration</a>
+          <a href="https://somesite.com/confirm-email?code=' + ${code} + '">complete registration</a>
       </p>`,
     });
   },
